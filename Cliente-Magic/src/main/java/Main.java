cdb43e60dcb5d92f086dbcbaeab4b3385d245e4c
Marco <93841465+Marco1080@users.noreply.github.com>
2024-12-05 17:57:47 +0000

    Se han importado dependencias en el pom de cliente para la API.
    https://docs.magicthegathering.io/

--- Cliente-Magic/src/main/java/Main.java
+++ Cliente-Magic/src/main/java/Main.java
@@ -1,5 +1,21 @@
+import io.magicthegathering.javasdk.api.CardAPI;
+import io.magicthegathering.javasdk.resource.Card;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        
+        /*
+        int multiverseId = 1;
+        Card card = CardAPI.getCard(multiverseId);
+        System.out.println(card);
+        */
+
+        List<Card> cards = CardAPI.getAllCards();
+        for (Card currentCard: cards) {
+            System.out.println(currentCard);
+        }
+
     }
 }
