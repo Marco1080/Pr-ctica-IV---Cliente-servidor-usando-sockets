b437c4d5b1c02202540c1bc729da1988a873872f
Daniel Delgado Pérez <danidp25.08@gmail.com>
2025-01-19 19:04:09 +0000

    a

--- Chat-ClienteServidor-JavaFX/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module org.example.chatclienteservidor {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens org.example.chatclienteservidor to javafx.fxml;
-    exports org.example.chatclienteservidor;
-}
